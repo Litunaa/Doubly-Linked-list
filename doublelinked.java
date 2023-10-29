9c767806786fe5b2fd9ebba4d9cd7d4e20096db2
litunaa <nubtem1337@gmail.com>
2023-10-29 19:32:51 +0100

    dada

--- doublelinked.java
+++ doublelinked.java
@@ -11,11 +11,59 @@ public class doublelinked<T>{
         this.tail = null;
     }
 
+    public int length(){
+        return this.length;
+    }
+
+    public T getFirst(){
+        return head.val();
+    }
+
+    public T getLast(){
+        return tail.val();
+    }
 
 
     public append(T data){
-        Node<T> newNode = new Node<T>()
+        Node<T> newNode = new Node<T>(data, this.tail, null);
+
+        if (this.tail == null){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+
+        else{
+            this.tail.setNext(newNode);
+            this.tail = newNode;
+        }
+        length++;
+
+    }
 
 
+    public String toString(){
+        Node<T> current  = this.head;
+
+        StringBuilder string = new StringBuilder();
+
+        while (current != null){
+            string.append(String.valueOf(current.val()) + " -> ");
+        }
+        string.append("null");
+
+        return string;
+    }
+
+
+
+
+    public static void main(String[] args){
+        doublelinked<Integer> list = new doublelinked<Integer>();
+
+        list.append(1);
+        list.append(5);
+
+
+        System.out.println(list.toString());
     }
 }
